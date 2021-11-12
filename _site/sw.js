4cb9f136fbf66daf87c35d8fd8f98bbe48449d47
양홍열 <drrd100@naver.com>
2021-11-12 16:17:20 +0900

    Initial commit

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211112.1454';
+const cacheName = 'chirpy-20211112.1514';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
