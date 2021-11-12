b1522328432a4ab0930806ac4887344aa4e5391b
양홍열 <drrd100@naver.com>
2021-11-12 15:44:06 +0900

    211112

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211112.1429';
+const cacheName = 'chirpy-20211112.1436';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
